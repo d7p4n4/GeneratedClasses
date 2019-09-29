6e59eaa4543b5086722855d40b94b88d481fc50e
d7p4n4 <bercikezs199999999@gmail.com>
2019-09-29 12:10:10 +0100

    Add project files.

---
+++ GeneratedClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeneratedClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
